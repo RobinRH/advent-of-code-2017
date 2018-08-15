96ec9e611fff53a7e3a9887e359014f91316b194
Robin Reynolds-Haertle <RobinRH@users.noreply.github.com>
2018-08-14 20:12:40 -0700

    Add files via upload

---
+++ day5-1.py
@@ -0,0 +1,29 @@
+#answer -> 388611
+import csv
+import sys
+
+print sys.argv[1]
+filename = sys.argv[1]
+
+total = 0
+instructions = []
+with open(filename, 'r') as f:
+    reader = csv.reader(f, delimiter=' ')
+    for row in reader:
+        total += 1
+        instructions.append(int(row[0]))
+
+#print instructions
+#print total
+
+
+nexti = 0
+steps = 0
+while nexti < total:
+    save = nexti
+    nexti = nexti + instructions[nexti]
+    instructions[save] += 1
+    steps += 1
+    #print instructions
+
+print steps
