04557d0a7b8675ffa9a8e2153c6aa04c370b948a
Robin Reynolds-Haertle <RobinRH@users.noreply.github.com>
2019-09-19 20:48:03 -0700

    Update day06.py

--- day06/day06.py
+++ day06/day06.py
@@ -1,62 +1,29 @@
-#answer part 1 -> 7864
-#answer part 2 -> 1695
+# answer -> 7864
+# answer -> 1695
 
-import csv
-import sys
+input = '0 5 10 0 11 14 13 4 11 8 8 7 1 4 12 11'
+input = [int(x) for x in input.split() if x]
 
-print sys.argv[1]
-filename = sys.argv[1]
+# part 1
+seen = []
+key1 = '-'.join([str(x) for x in input])
+seen.append(key1)
 
-# row1 is test input, row2 is real input
-banks = []
-with open(filename, 'r') as f:
-    reader = csv.reader(f, delimiter='\t')
-    for row in reader:
-        banks = list(map(lambda x: int(x), row))
+while len(seen) == len(set(seen)):
 
-print banks
-states = []
-state = ""
-states.append(reduce((lambda x, y: str(x) + "-" + str(y)), banks))
-foundRepeat = False
-count = 0
-while (not foundRepeat):
-    # find the back with the most blocks
-    maxBank = (0, 0) # tuple is number of blocks and bank index
+    maxblocks = max(input)
+    index = input.index(maxblocks)
+    input[index] = 0
+    start = index + 1
+    for i in range(maxblocks):
+        location = (start + i) % len(input)
+        input[location] += 1
 
-    for i in range(len(banks)):
-        if banks[i] > maxBank[0]:
-            maxBank = (banks[i], i)
+    key = '-'.join([str(x) for x in input])
+    seen.append(key)
 
-    print maxBank
+print('part 1: ', len(seen) - 1)
 
-    # redistribute the blocks
-    banks[maxBank[1]] = 0
-    location = maxBank[1] + 1
-    for block in range(maxBank[0]):
-        if (location == len(banks)): location = 0
-        banks[location] += 1
-        location += 1
-
-    # change to string and join
-    state = reduce((lambda x, y: str(x) + "-" + str(y)), banks)
-    states.append(state)
-
-    print state
-    print count
-    count += 1
-    if count > 10000: exit()
-
-    # find out if there are any duplicates in the states
-    unique = set(states)
-    if (len(states) != len(unique)):
-        foundRepeat = True
-
-print "Answer part 1: ", count
-
-# find out the two locations of the repeat
-firstIndex = states.index(state)
-lastIndex = count
-loopsize =  lastIndex - firstIndex
-print "Answer part 2, loop size: " + str(loopsize)
-
+lastkey = seen[-1]
+firstkey = seen.index(lastkey)
+print('part 2: ', len(seen) - 1 - firstkey)
